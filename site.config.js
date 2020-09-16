1cc1d4ac43fafd2be2a5be6dda4d3fb7436ba955
billyhkim <billyhkim.dev@gmail.com>
2020-09-15 17:22:12 -0700

    add site config

---
+++ site.config.js
@@ -0,0 +1,17 @@
+export default {
+  siteMetadata: {
+    title: 'Billy H. Kim',
+    author: {
+      name: 'Billy H. Kim',
+      summary: 'who builds with React.',
+    },
+    description: 'A portfolio created with Next.js and Tailwind.css',
+    siteUrl: 'https://billyhkim.com/',
+    language: 'en-US',
+    social: {
+      twitter: 'billyhyunkim',
+      github: 'billyhkim',
+      linkedin: 'billyhkim',
+    },
+  },
+};
