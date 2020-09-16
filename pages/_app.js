96bfdf6249eab77d135c284fdec20b6e98cf580f
billyhkim <billyhkim.dev@gmail.com>
2020-09-15 17:21:51 -0700

    wrap top layer with framer + body border

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,48 @@
-import '../styles/globals.css'
+import { useEffect } from 'react';
+import { motion } from 'framer-motion';
+import Layout from '../components/Layout';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/globals.css';
+import '../styles/tailwind.css';
+import 'typeface-inter';
+
+function MyApp({ Component, pageProps, router }) {
+  const MotionWrappedApp = () => (
+    <motion.div
+      key={router.route}
+      initial="start"
+      animate="end"
+      variants={{
+        start: {
+          opacity: 0,
+        },
+        end: {
+          opacity: 1,
+          transition: { duration: 0.4 },
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </motion.div>
+  );
+
+  return (
+    <>
+      {router.route !== '/' ? (
+        <Layout>
+          <MotionWrappedApp />
+        </Layout>
+      ) : (
+        <MotionWrappedApp />
+      )}
+
+      {/* Body Borders */}
+      <div id="body-border-left" />
+      <div id="body-border-right" />
+      <div id="body-border-top" />
+      <div id="body-border-bottom" />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
